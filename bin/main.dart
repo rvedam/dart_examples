542d601d020efded82e66b23eda234b9759d6ccd
Ramnarayan Vedam <rvedam@gmail.com>
2015-03-06 01:10:27 -0500

    Moving code around for better organization

--- bin/main.dart
+++ bin/main.dart
@@ -1,39 +1,7 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// Copyright (c) 2015, Ram Vedam. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:dart_examples/dart_examples.dart' as dart_examples;
-
-// no types
-reduce(from, to, afun) {
-  var result = 0.0;
-  for(var i = from; i < to; i++) {
-    result += afun(i);
-  }
-  return result;
-}
-
-// optionally named parameters
-num sum({int from, int to, Function afun}) {
-  var result = 0.0;
-  for (int i = from; i < to; i++) {
-    result += afun(i);
-  }
-  return result;
-}
-
-// example of function with positional arguments
-num mysum(int from, int to, [Function afun]) {
-  num result = 0.0;
-  var fun;
-  if (afun != null) {
-    fun = afun;
-  }
-  for (int i = from; i < to; i++) {
-    result += fun(i);
-  }
-
-  return result;
-}
 
 class Point {
   num _x;
--- lib/dart_examples.dart
+++ lib/dart_examples.dart
@@ -1,17 +1,37 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// Copyright (c) 2015, Ram Vedam. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 /// The dart_examples library.
 library dart_examples;
 
-int calculate() {
-  return 6 * 7;
+// no types
+reduce(from, to, afun) {
+var result = 0.0;
+for(var i = from; i < to; i++) {
+ result += afun(i);
+}
+return result;
 }
 
-bool myequal(a,b) {
-  return a == b;
+// optionally named parameters
+num sum({int from, int to, Function afun}) {
+var result = 0.0;
+for (int i = from; i < to; i++) {
+ result += afun(i);
+}
+return result;
 }
 
-bool myidentical(a,b) {
-  return a.identical(b);
+// example of function with positional arguments
+num mysum(int from, int to, [Function afun]) {
+num result = 0.0;
+var fun;
+if (afun != null) {
+ fun = afun;
+}
+for (int i = from; i < to; i++) {
+ result += fun(i);
+}
+
+return result;
 }
