512f89987bfb7324e2be5912bebeedf01f5d7ea2
Ramnarayan Vedam <rvedam@gmail.com>
2015-03-06 10:21:25 -0500

    added square function to dart_examples

--- lib/dart_examples.dart
+++ lib/dart_examples.dart
@@ -4,34 +4,52 @@
 /// The dart_examples library.
 library dart_examples;
 
+// example function
+int calculate() {
+  return 6 * 7;
+}
+
 // no types
 reduce(from, to, afun) {
-var result = 0.0;
-for(var i = from; i < to; i++) {
- result += afun(i);
-}
-return result;
+  var result = 0.0;
+  for (var i = from; i < to; i++) {
+    result += afun(i);
+  }
+  return result;
 }
 
-// optionally named parameters
+// typed function with optionally named parameters
 num sum({int from, int to, Function afun}) {
-var result = 0.0;
-for (int i = from; i < to; i++) {
- result += afun(i);
-}
-return result;
+  var result = 0.0;
+  for (int i = from; i < to; i++) {
+    result += afun(i);
+  }
+  return result;
 }
 
-// example of function with positional arguments
+// typed function with positional arguments
 num mysum(int from, int to, [Function afun]) {
-num result = 0.0;
-var fun;
-if (afun != null) {
- fun = afun;
-}
-for (int i = from; i < to; i++) {
- result += fun(i);
+  num result = 0.0;
+  var fun;
+  if (afun != null) {
+    fun = afun;
+  }
+  for (int i = from; i < to; i++) {
+    result += fun(i);
+  }
+
+  return result;
 }
 
-return result;
+// mix-and-match example
+product(int from, int to, {Function fun}) {
+  var result = 1.0;
+  for(var i = from; i < to; i++) {
+    result *= fun(i);
+  }
+  return result;
+}
+
+square(num x) {
+  return x * x;
 }
