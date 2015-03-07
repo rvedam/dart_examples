1c0ef6b5afd46ed09563f20f3377771ab4fd45bf
Ramnarayan Vedam <rvedam@gmail.com>
2015-03-07 18:47:25 -0500

    Added different ways of defining top-level main function for a dart program

--- bin/main.dart
+++ bin/main.dart
@@ -3,9 +3,16 @@
 
 import 'package:dart_examples/dart_examples.dart' as dart_examples;
 
-// position parameters
-main() {
-  print('Hello world: ${dart_examples.calculate()}!');
-  assert(mysum(1, 10, (x) => x) == sum(from: 1, to: 10, afun: (x) => x));
-  
+//main() {
+//  print('Hello world: ${dart_examples.calculate()}!');
+//  assert(mysum(1, 10, (x) => x) == sum(from: 1, to: 10, afun: (x) => x));
+//  
+//}
+
+//main(args) {
+//  print(args.length);
+//}
+
+main(List<String> args) {
+  print('number of arguments passed ${args.length}');
 }
