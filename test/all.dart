47bdc45f3334daab36ce36f9785ee2ff322dc272
Ramnarayan Vedam <vedam@velocitymedical.com>
2015-03-10 10:57:39 -0400

    Fixed test suite for moving mixin_examples to class_examples

--- test/all.dart
+++ test/all.dart
@@ -4,9 +4,9 @@
 library all_tests;
 
 import 'dart_examples_test.dart' as dart_examples_test;
-import 'mixin_examples_test.dart' as mixin_examples_test;
+import 'class_examples_test.dart' as class_examples_test;
 
 void main() {
   dart_examples_test.defineTests();
-  mixin_examples_test.defineTests();
+  class_examples_test.defineTests();
 }
