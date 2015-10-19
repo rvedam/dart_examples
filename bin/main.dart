755394e401ef83692ce3d8cfce1f6334a536e45e
Ram Vedam <rvedam@gmail.com>
2015-10-18 21:06:51 -0400

    Fixed issue with generic adder function. now instead of using an alias, it just simply returns a function. Also, updated main example to use relative imports

--- bin/main.dart
+++ bin/main.dart
@@ -1,12 +1,12 @@
 // Copyright (c) 2015, Ram Vedam. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:dart_examples/dart_examples.dart' as dart_examples;
+import '../lib/dart_examples.dart' as dart_examples;
 
 //main() {
 //  print('Hello world: ${dart_examples.calculate()}!');
 //  assert(mysum(1, 10, (x) => x) == sum(from: 1, to: 10, afun: (x) => x));
-//  
+//
 //}
 
 //main(args) {
@@ -15,27 +15,39 @@ import 'package:dart_examples/dart_examples.dart' as dart_examples;
 
 typedef num AdderFunc(num a, num b);
 
-AdderFunc createAdder(num a) {
-  Function adder = (num x, [num b]) {
-    return x + b;
-  };
-  
-  return adder(a);
+//
+// example of closures
+//
+// This example creates generic adders that take in an argument
+// and returns an anonymoous function that takes in a argument and returns the sum of
+// that argument and the internal argument was "wrapped into" the function when it was
+// constructed.
+//
+Function createAdder(num a) {
+  return ((num x) => x + a);
 }
 
-main(List<String> args) {
+void main(List<String> args) {
+  // uses string interpolation to evaluate expression and auto convert to a string.
   print('number of arguments passed ${args.length}');
+  try {
+    var add2 = createAdder(2);
+    print(add2(2));
+  } catch(e) {
+    print('Error found');
+  }
+  print('-------------------------------\n');
+  print('Mixin example\n');
+  print('-------------------------------\n');
   var gym = new dart_examples.Gym();
   var dojo = new dart_examples.Dojo();
-  var add2 = createAdder(2);
-  print(add2(2));
-  
   print('Exercising @ Gym');
   gym.pushUps();
   gym.preacherCurls();
   print('\n');
-  
+
   print('Exercising @ Dojo');
   dojo.pushUps();
   dojo.tai_kyo_ku();
+  print('\n');
 }
